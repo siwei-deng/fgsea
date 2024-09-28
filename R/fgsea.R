57ba2d6e692e4c3c214c48a106503b4399e1dcae
Siwei Deng <54531483+siwei-deng@users.noreply.github.com>
2024-09-28 11:35:50 +0100

    Update fgsea.R

--- R/fgsea.R
+++ R/fgsea.R
@@ -757,7 +757,7 @@ fgseaSimpleImpl <- function(pathwayScores, pathwaysSizes,
     
     # Remove the intermediate ES_size_norm column
     pvals[, ES_size_norm := NULL]
-    pvals[, NES_raw := NULL]
+    # pvals[, NES_raw := NULL]
 
     ### END
     
